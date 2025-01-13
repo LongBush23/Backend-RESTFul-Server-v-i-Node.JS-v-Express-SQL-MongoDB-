cbeea93b3a4c7e3e37c7ad945d651b561e407fbe
Nguyễn Long <nguyenlong@MacBook-Air-cua-Nguyen.local>
2025-01-13 21:06:42 +0700

    setup docker and connect database

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express"); //commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const connection = require("./config/dataBase");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; //port => hardcode . uat .prod
@@ -12,6 +13,12 @@ configViewEngine(app);
 
 //khai báo route
 app.use("/", webRoutes);
+
+//test connection
+// simple query
+connection.query("select * from Users u", function (err, results, fields) {
+  console.log(">>>results= ", results); // results contains rows returned by server
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);

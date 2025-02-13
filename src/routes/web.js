e4c805b25fba5fc075cb1ccf7548ceee67262ae2
Nguyễn Long <nguyenlong@MacBook-Air-cua-Nguyen.local>
2025-02-13 23:58:00 +0700

    queryListUsers

--- src/routes/web.js
+++ src/routes/web.js
@@ -12,9 +12,7 @@ const router = express.Router();
 router.get("/", getHomepage);
 router.get("/abc", getABC);
 router.get("/hoidanit", getHoiDanIT);
-
 router.get("/create", getCreatePage);
-
 router.post("/create-user", postCreateUser);
 
 module.exports = router; //export default

8bb95d65eb7d89e151b0777be1f7c9eee8ea80b0
Nguyễn Long <nguyenlong@MacBook-Air-cua-Nguyen.local>
2025-02-16 17:43:45 +0700

    CRUD sql

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,10 @@
 const connection = require("../config/database");
-const { getAllUsers } = require("../services/CRUDService");
+const {
+  getAllUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+} = require("../services/CRUDService");
 
 const getHomepage = async (req, res) => {
   let results = await getAllUsers();
@@ -33,10 +38,42 @@ const getCreatePage = (req, res) => {
   res.render("create.ejs");
 };
 
+const getUpdatePage = async (req, res) => {
+  const userId = req.params.id;
+  let user = await getUserById(userId);
+  res.render("edit.ejs", { userEdit: user });
+};
+
+const postUpdateUser = async (req, res) => {
+  let email = req.body.email;
+  let name = req.body.myname;
+  let city = req.body.city;
+  let userId = req.body.userId;
+  await updateUserById(email, city, name, userId);
+  // res.send(' Updated user succeed !')
+  res.redirect("/");
+};
+
+const postDeleteUser = async (req, res) => {
+  const userId = req.params.id;
+  let user = await getUserById(userId);
+  res.render("delete.ejs", { userEdit: user });
+};
+
+const postHandleRemoveUser = async (req, res) => {
+  const id = req.body.userId;
+  await deleteUserById(id);
+  res.redirect("/");
+};
 module.exports = {
   getHomepage,
   getABC,
   getHoiDanIT,
-  getCreatePage,
   postCreateUser,
+  getCreatePage,
+  getUpdatePage,
+  postUpdateUser,
+  postDeleteUser,
+  postHandleRemoveUser,
+  deleteUserById,
 };
--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -5,6 +5,35 @@ const getAllUsers = async () => {
   return results;
 };
 
+const getUserById = async (userId) => {
+  let [results, fields] = await connection.query(
+    "SELECT * FROM Users u where id = ? ",
+    [userId]
+  );
+  let user = results && results.length > 0 ? results[0] : {};
+  return user;
+};
+
+const updateUserById = async (email, city, name, userId) => {
+  let [results, fields] = await connection.query(
+    `
+      UPDATE Users u
+      SET email = ?, city= ?, name= ?
+      WHERE id = ?
+      `,
+    [email, city, name, userId]
+  );
+};
+
+const deleteUserById = async (id) => {
+  let [results, fields] = await connection.query(
+    ` DELETE FROM Users WHERE id=? `,
+    [id]
+  );
+};
 module.exports = {
   getAllUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById,
 };
